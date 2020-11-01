aea9aec7d7026934124dfb70c5a16acf59235a57
farahzuot <farahz997@eng.hu.edu.jo>
2020-11-01 15:53:09 +0200

    lab11 part1

--- server.js
+++ server.js
@@ -2,17 +2,27 @@
 
 const express = require('express');
 const cors = require('cors');
+const ejs = require('ejs');
 const superagent = require('superagent');
 require('dotenv').config;
 
-const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
 
 
+// Setting the view engine for templating
+app.set('view engine', 'ejs');
+
+// Middleware (access the data form (Form Data header))
+//app.use(express.urlencoded({ extended: true }));
+//app.use(express.static('public'));
+
+const PORT = process.env.PORT || 3000;
+
+
 app.get('/' , (req,res)=> {
-  res.status(200).send('WELCOME!')
+  res.render('pages/index');
 })
 
 app.use('*' , (req,res)=>{
